baa78cdd2b5ecc67fa3ef7c7789e4bccb51b44dd
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-07-19 17:40:12 +0300

    güncellendi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import Profile from './components/Profile';
+import Skills from './components/Skills';
+import Info from './components/Info';
+import Header from './components/Header';
+import useDarkMode from './hooks/useDarkMode';
+import useLanguageChange from './hooks/useLanguageChange';
+import Mode from './components/Mode';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  const [ darkMode, setDarkMode ] = useDarkMode("darkMode", false)
+  const [ language, setLanguage ] = useLanguageChange("language", "en")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${darkMode ? "dark bg-[#2A262B]" : "App" }`}>
+      <Mode darkMode={darkMode} setDarkMode={setDarkMode} language={language} setLanguage={setLanguage} />
+      <Header language={language}/>
+      <Info language={language}/>
+      <Skills language={language}/>
+      <Profile language={language}/>
+      <Projects language={language}/>
+      <Footer language={language}/>
     </div>
   );
 }
