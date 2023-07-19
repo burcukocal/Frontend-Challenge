e52e901a1a9231b8056ed741659be542be4a9977
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-07-19 17:39:15 +0300

    teme ve dil eklendi

---
+++ src/components/Mode.js
@@ -0,0 +1,55 @@
+import { data } from "../data/data";
+
+const Mode = ({ darkMode, setDarkMode, language, setLanguage}) => {
+
+    const toggleMode = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode);
+    };
+
+    const changeLanguage = () => {
+        const newLanguage = language === "en" ? "tr" : "en";
+        setLanguage(newLanguage)
+      }
+
+    return (
+        <div className="modes">
+            <div className="dark-mode">
+                <button
+                    onClick={toggleMode}
+                    className={darkMode ? 'toggle toggled' : 'toggle'}
+                />
+                <button className={darkMode ? 'two' : ''}></button>                
+            </div>
+            <div className="mode-p">
+            {language === "en" ? 
+            (
+                <p className="font-bold text-sm inline-block text-[#6B7280]"> {`${darkMode ? data.en.lightMode : data.en.darkMode} `} </p>) 
+            : (
+                <p className="font-bold text-sm inline-block text-[#6B7280]"> {`${darkMode ? data.tr.lightMode : data.tr.darkMode}`} </p>
+            )
+            }
+            </div>
+            <div className="line">
+            <p className="text-sm text-[#6B7280] font-bold inline-block"> | </p>
+            </div>
+
+            {language === "en" ? (
+                <div className="language" onClick={changeLanguage}>
+                <p  className="text-[#4731D3] font-bold text-sm inline-block">TÜRKÇE</p>
+                <span className="text-[#777777] font-bold text-sm">'YE GEÇ</span>              
+            </div>
+             ): (<div className="language" onClick={changeLanguage}>
+                <span className="text-[#777777] font-bold text-sm px-2">TURN</span>
+            <p  className="text-[#4731D3] font-bold text-sm inline-block">ENGLISH</p>
+          
+        </div>)}
+            
+            
+
+        </div>
+    )
+
+};
+
+export default Mode;
