7f019224f49d1686d3654d88e203d49e5345571e
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-07-20 18:07:38 +0300

    proje datası hazırlandı

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,16 +1,16 @@
 export const projectList = {
     "en":
         [{
-            img: "shopping-cart.png",
-            title: "Shopping Cart",
-            text: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes and type of customization to code and themes possible.",
-            contents: ["react", "axios"],
-            url1: "https://github.com/burcukocal/fsweb-s11g1-shopping-cart",
-            url2: "https://shopping-cart-ten-teal.vercel.app/",
+            img: "redux-watchlist.png",
+            title: "Watch List",
+            text: "Watch List is a react project that allows navigating movies and creating lists. You can see the movies added to the list and remove movies from the list. Redux, redux-logger and reducer are used in project.",
+            contents: ["react", "axios", "redux"],
+            url1: "https://github.com/burcukocal/fsweb-s10g3-redux-watchlist",
+            url2: "https://redux-watchlist-xi.vercel.app/",
         }, {
             img: "minnet-gunlugu.png",
             title: "Gratitude Journal",
-            text: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes and type of customization to code and themes possible.",
+            text: "A gratitude diary is a kind of diary where one often writes down things to be thankful for so that they can focus on the positive things in their life and feel a sense of gratitude and gratitude. Previously added notes are listed, notes can be deleted and new notes can be added. Redux-thunk is used in the project.",
             contents: ["react", "axios", "redux"],
             url1: "https://github.com/burcukocal/fsweb-s10-challenge",
             url2: "https://minnet-gunlugu-roan.vercel.app/",
@@ -18,23 +18,23 @@ export const projectList = {
         {
             img: "random-jokes.png",
             title: "Random Jokes",
-            text: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes and type of customization to code and themes possible.",
+            text: "Random Jokes is a react project that returns jokes and answers. You can add and remove jokes from your favorites. Redux, redux-thunk, localstorage ve toastify are used in the project",
             contents: ["react", "axios", "redux"],
             url1: "https://github.com/burcukocal/fsweb-s10g4-thunk-and-apis",
             url2: "https://random-jokes-chi.vercel.app/",
         }],
     "tr":
         [{
-            img: "shopping-cart.png",
-            title: "Alışveriş Kartı",
-            text: "Kullanıcılarınızın hangi tanımlama bilgilerini kabul edeceklerini veya reddedeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimum kurulumlu bir tanımlama bilgisi eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kodlama ve temalar için özelleştirme türü ve türünü mümkün kılar.",
-            contents: ["react", "axios"],
-            url1: "https://github.com/burcukocal/fsweb-s11g1-shopping-cart",
-            url2: "https://shopping-cart-ten-teal.vercel.app/",
+            img: "redux-watchlist.png",
+            title: "İzlenecekler Listesi",
+            text: "İzlenecekler listesi, filmler arasında gezinmeyi ve liste oluşturmayı sağlayan bir react projesidir. Listeye eklenen filmleri görebilir ve listeden film çıkarabilirisiniz. Projede redux, redux-logger ve reducer kullanılmıştır",
+            contents: ["react", "axios", "redux"],
+            url1: "https://github.com/burcukocal/fsweb-s10g3-redux-watchlist",
+            url2: "https://redux-watchlist-xi.vercel.app/",
         }, {
             img: "minnet-gunlugu.png",
             title: "Minnet Günlüğü",
-            text: "Kullanıcılarınızın hangi tanımlama bilgilerini kabul edeceklerini veya reddedeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimum kurulumlu bir tanımlama bilgisi eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kodlama ve temalar için özelleştirme türü ve türünü mümkün kılar.",
+            text: "Minnet günlüğü, kişinin hayatındaki olumlu şeylere odaklanması, minnettarlık ve şükran duygusunu hissetmesi için sık sık teşekkür ettiği şeyleri yazdığı bir tür günlüktür. Daha önce eklenmiş notlar listelenir, notlar silinebilir ve yeni notlar eklenebilmektedir. Projede Redux-thunk kullanılmıştır.",
             contents: ["react", "axios", "redux"],
             url1: "https://github.com/burcukocal/fsweb-s10-challenge",
             url2: "https://minnet-gunlugu-roan.vercel.app/",
@@ -42,8 +42,8 @@ export const projectList = {
         {
             img: "random-jokes.png",
             title: "Rastgele Şakalar",
-            text: "Kullanıcılarınızın hangi tanımlama bilgilerini kabul edeceklerini veya reddedeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimum kurulumlu bir tanımlama bilgisi eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kodlama ve temalar için özelleştirme türü ve türünü mümkün kılar.",
-            contents: ["react", "axios", "redux"],
+            text: "Rastgele Şakalar, şaka ve cevaplarını döndüren bir react projesidir. Şakaları favorilerine ekleyebilir ve çıkarabilirsin. Projede redux, redux-thunk, localstorage ve toastify kullanılmıştır",
+            contents: ["react", "axios", "redux", "toastify"],
             url1: "https://github.com/burcukocal/fsweb-s10g4-thunk-and-apis",
             url2: "https://random-jokes-chi.vercel.app/",
         }]
